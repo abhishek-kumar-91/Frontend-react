735ccc2990f0612554578608f5eb65a27918fbf5
Abhishek_Kumar <abhishekkumar958830@gmail.com>
2023-09-14 14:30:32 +0530

    react router

---
+++ reactRouter/src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+function Footer() {
+  return (
+    <>
+    <div className='flex w-full bg-gray-200 px-5 items-center h-64 justify-evenly'>
+    <div className='flex gap-1'>
+                <h1 className='text-white text-5xl font-extrabold'>Your</h1><h1 className='text-orange-500 text-3xl font-bold'>Logo</h1>
+            </div>
+    <div>
+        
+    </div>
+    <ul className='flex flex-col gap-4 text-md font-md text-gray-500'>
+                    <li className='font-bold'> Pages </li>
+                    <li className='hover:text-orange-500 cursor-pointer'><Link to="/">Home</Link></li>
+                    <li className='hover:text-orange-500 cursor-pointer'><Link to="about">About</Link></li>
+                    <li className='hover:text-orange-500 cursor-pointer'><Link to="contact-us">Contact Us</Link></li>
+                    <li className='hover:text-orange-500 cursor-pointer'><Link to="github">Github</Link></li>
+                </ul>
+    <div>
+    <ul className='flex flex-col gap-4 text-md font-md text-gray-500'>
+                    <li className='font-bold'>Help</li>
+                    <li className='hover:text-orange-500 cursor-pointer'>FAQ</li>
+                    <li className='hover:text-orange-500 cursor-pointer'>Customer Care</li>
+                    <li className='hover:text-orange-500 cursor-pointer'>Complaint</li>
+                    <li className='hover:text-orange-500 cursor-pointer'>Address</li>
+                </ul>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ reactRouter/src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+function Header() {
+  return (
+    <>
+        <div className='flex w-full h-16 bg-gray-200 justify-between px-5 items-center'>
+            <div className='flex gap-1'>
+             <Link to="/"><h1 className='text-white text-xl cursor-pointer font-extrabold'>Your</h1><h1 className='text-orange-500 font-bold'>Logo</h1></Link>   
+            </div>
+
+            <div>
+                <ul className='flex gap-8 text-lg font-md text-gray-500'>
+                  <li>
+                    <NavLink to="/" className={({isActive})=>`${isActive ? "text-orange-500" : "text-gray-500"} hover:text-orange-500 cursor-pointer`}>
+                    Home 
+                    </NavLink>
+                    </li> 
+                    <li>
+                    <NavLink to="about" className={({isActive})=>`${isActive ? "text-orange-500" : "text-gray-500"} hover:text-orange-500 cursor-pointer`}>
+                    About 
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="contact-us" className={({isActive})=>`${isActive ? "text-orange-500" : "text-gray-500"} hover:text-orange-500 cursor-pointer`}>
+                    Contact US 
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="github" className={({isActive})=>`${isActive ? "text-orange-500" : "text-gray-500"} hover:text-orange-500 cursor-pointer`}>
+                   Github 
+                    </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='flex gap-3'>
+                <button className='hover:text-orange-500 ease-in-out duration-300 cursor-pointer text-gray-500 font-semibold text-xl'>LogIn</button>
+                <button className='bg-orange-500 p-2 rounded-md  text-white hover:bg-white hover:border-solid hover:border-2 hover:border-orange-500 hover:text-gray-500 ease-in-out duration-300'>SignUp</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
